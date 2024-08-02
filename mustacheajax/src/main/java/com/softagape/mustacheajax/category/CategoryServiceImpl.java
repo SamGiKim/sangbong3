cdedadf28a679075daa357831453bf14b9be7f20
SoftAgape, Sincerus <sincerus@hanmail.net>
2024-08-02 11:05:02 +0900

    주석 작업 진행
    주석 작업 진행

--- mustacheajax/src/main/java/com/softagape/mustacheajax/category/CategoryServiceImpl.java
+++ mustacheajax/src/main/java/com/softagape/mustacheajax/category/CategoryServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements ICategoryService<ICategory> {
-    @Autowired
+    @Autowired  // SpringBoot 가 CategoryMybatisMapper 데이터형으로 객체를 자동 생성한다.
     private CategoryMybatisMapper categoryMybatisMapper;
 
     @Override
@@ -17,6 +17,7 @@ public class CategoryServiceImpl implements ICategoryService<ICategory> {
             return null;
         }
         CategoryDto find = this.categoryMybatisMapper.findById(id);
+        // CategoryMybatisMapper 의 쿼리 XML 파일의 <select id="findById" 문장을 실행한 결과를 리턴 받는다.
         return find;
     }
 
@@ -26,6 +27,7 @@ public class CategoryServiceImpl implements ICategoryService<ICategory> {
             return null;
         }
         CategoryDto find = this.categoryMybatisMapper.findByName(name);
+        // CategoryMybatisMapper 의 쿼리 XML 파일의 <select id="findByName" 문장을 실행한 결과를 리턴 받는다.
         return find;
     }
 
@@ -33,6 +35,7 @@ public class CategoryServiceImpl implements ICategoryService<ICategory> {
     public List<ICategory> getAllList() {
         List<ICategory> list = this.getICategoryList(
                 this.categoryMybatisMapper.findAll()
+                // CategoryMybatisMapper 의 쿼리 XML 파일의 <select id="findAll" 문장을 실행한 결과를 리턴 받는다.
         );
         return list;
     }
@@ -46,10 +49,14 @@ public class CategoryServiceImpl implements ICategoryService<ICategory> {
 //        for( CategoryDto entity : list ) {
 //            result.add( (ICategory)entity );
 //        }
+        // output : [ICategory|ICategory|ICategory|ICategory|ICategory]
         List<ICategory> result = list.stream()
                 .map(entity -> (ICategory)entity)
                 .toList();
-        // return : [ICategory|ICategory|ICategory|ICategory|ICategory]
+        // stream Java 1.8 등장한 방법 : 배열/Collection 자료형을 처리할때
+        // stream 을 사용하여 처리 속도도 증가하고 문법도 간결하게 만든다.
+        // 배열객체/컬렉션자료형.stream() ~~~~ 정렬, 형변환, 원소마다 똑같은 동작을 처리한다.
+
         return result;
     }
 
@@ -62,6 +69,8 @@ public class CategoryServiceImpl implements ICategoryService<ICategory> {
         dto.copyFields(category);
         dto.setId(0L);
         this.categoryMybatisMapper.insert(dto);
+        // CategoryMybatisMapper 의 쿼리 XML 파일의 <insert id="insert" 문장을 실행한다.
+        // dto.id 는 자동증가된 id 값이 리턴된다.
         return dto;
     }
 
@@ -81,6 +90,7 @@ public class CategoryServiceImpl implements ICategoryService<ICategory> {
             return false;
         }
         this.categoryMybatisMapper.deleteById(id);
+        // CategoryMybatisMapper 의 쿼리 XML 파일의 <delete id="deleteById" 문장을 실행한다.
         return true;
     }
 
@@ -91,7 +101,8 @@ public class CategoryServiceImpl implements ICategoryService<ICategory> {
             return null;
         }
         find.copyFields(category);
-        this.categoryMybatisMapper.update((CategoryDto)find);
+        this.categoryMybatisMapper.update((CategoryDto) find);
+        // CategoryMybatisMapper 의 쿼리 XML 파일의 <update id="update" 문장을 실행한다.
         return find;
     }
 
@@ -101,18 +112,16 @@ public class CategoryServiceImpl implements ICategoryService<ICategory> {
             //return List.of();
             return new ArrayList<>();
         }
-        String sOrer = "";
-        if ( dto.getSortColumn() == null ) {
-            sOrer = "id";
-        } else {
-            sOrer = dto.getSortColumn();
-        }
-        dto.setOrderByWord( sOrer + " " + dto.getSortAscDsc() );
+        dto.setOrderByWord( (dto.getSortColumn() != null ? dto.getSortColumn() : "id")
+                + " " + (dto.getSortAscDsc() != null ? dto.getSortAscDsc() : "DESC") );
+        // SQL select 문장의 ORDER BY 구문을 만들어 주는 역할
         if ( dto.getRowsOnePage() == null ) {
+            // 한 페이지당 보여주는 행의 갯수
             dto.setRowsOnePage(10);
         }
         List<ICategory> list = this.getICategoryList(
                 this.categoryMybatisMapper.findAllByNameContains(dto)
+                // CategoryMybatisMapper 의 쿼리 XML 파일의 <select id="findAllByNameContains" 문장을 실행한 결과를 리턴한다.
         );
         return list;
     }
@@ -120,5 +129,6 @@ public class CategoryServiceImpl implements ICategoryService<ICategory> {
     @Override
     public int countAllByNameContains(SearchCategoryDto searchCategoryDto) {
         return this.categoryMybatisMapper.countAllByNameContains(searchCategoryDto);
+        // CategoryMybatisMapper 의 쿼리 XML 파일의 <select id="categoryMybatisMapper" 문장을 실행한 결과를 리턴한다.
     }
 }
