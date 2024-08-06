ecdc63c3951b5684bf14baa8b454d6815858bfb7
SoftAgape, Sincerus <sincerus@hanmail.net>
2024-08-06 12:44:16 +0900

    add login

--- mustacheajax/src/main/java/com/softagape/mustacheajax/member/IMemberService.java
+++ mustacheajax/src/main/java/com/softagape/mustacheajax/member/IMemberService.java
@@ -2,11 +2,13 @@ package com.softagape.mustacheajax.member;
 
 import com.softagape.mustacheajax.ICommonService;
 import com.softagape.mustacheajax.SearchAjaxDto;
+import com.softagape.mustacheajax.security.dto.LoginRequest;
 import com.softagape.mustacheajax.security.dto.SignUpRequest;
 
 import java.util.List;
 
 public interface IMemberService extends ICommonService<MemberDto> {
+    IMember login(LoginRequest dto);
     IMember addMember(SignUpRequest dto);
     IMember findByLoginId(String loginId);
     List<IMember> findAllByLoginIdContains(SearchAjaxDto dto);
--- mustacheajax/src/main/java/com/softagape/mustacheajax/security/controller/LoginCookieController.java
+++ mustacheajax/src/main/java/com/softagape/mustacheajax/security/controller/LoginCookieController.java
@@ -1,10 +1,13 @@
 package com.softagape.mustacheajax.security.controller;
 
+import com.softagape.mustacheajax.member.IMember;
 import com.softagape.mustacheajax.member.IMemberService;
+import com.softagape.mustacheajax.security.dto.LoginRequest;
 import com.softagape.mustacheajax.security.dto.SignUpRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,5 +41,27 @@ public class LoginCookieController {
             log.error(ex.toString());
         }
         return "redirect:/cologin";
+    }
+
+    @GetMapping("/login")
+    private String viewLogin() {
+        return "login/login";
+    }
+
+    @PostMapping("/signin")
+    private String signin(Model model, @ModelAttribute LoginRequest dto) {
+        try {
+            if (dto == null) {
+                return "redirect:/cologin";
+            }
+            IMember loginUser = this.memberService.login(dto);
+            if ( loginUser != null ) {
+                model.addAttribute("loginUser", loginUser);
+                return "user/info";
+            }
+        } catch (Exception ex) {
+            log.error(ex.toString());
+        }
+        return "login/fail";
     }
 }
